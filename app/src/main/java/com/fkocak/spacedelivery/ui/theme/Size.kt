25a409f79074cc93414f79941454ef2a14840f93
fatihkkocak <fatih@marketyo.com>
2022-05-13 15:37:20 +0300

    Added Font and coded Global Size Values.
    - Added poppins font for this project
    - Coded Size value for any components

---
+++ app/src/main/java/com/fkocak/spacedelivery/ui/theme/Size.kt
@@ -0,0 +1,23 @@
+package com.fkocak.spacedelivery.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * Distance
+ */
+var DISTANCE_CENTER_IMAGE = 170.dp
+var DISTANCE_LETTERS_FROM_CENTER = 160.dp
+var DISTANCE_PONTER_FROM_CENTER = 130.dp
+val DISTANCE_FIRST_CHAR_FROM_CENTER = 90.dp
+
+/**
+ * Text Size
+ */
+val TEXT_SIZE_10 = 10.sp
+val TEXT_SIZE_12 = 12.sp
+val TEXT_SIZE_14 = 14.sp
+val TEXT_SIZE_15 = 15.sp
+val TEXT_SIZE_16 = 16.sp
+val TEXT_SIZE_18 = 18.sp
+val TEXT_SIZE_20 = 20.sp
--- app/src/main/java/com/fkocak/spacedelivery/ui/theme/Type.kt
+++ app/src/main/java/com/fkocak/spacedelivery/ui/theme/Type.kt
@@ -2,27 +2,63 @@ package com.fkocak.spacedelivery.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.fkocak.spacedelivery.R
 
-// Set of Material typography styles to start with
+object TypeOfFont {
+
+    fun poppinsMediumStyle(textUnit: TextUnit, align: TextAlign? = TextAlign.Start): TextStyle {
+        return Typography(
+            h1 = TextStyle(
+                fontFamily = PoppinsMedium,
+                fontSize = textUnit,
+                textAlign = align
+            )
+        ).h1
+    }
+
+    fun poppinsSemiBoldStyle(textUnit: TextUnit, align: TextAlign? = TextAlign.Start): TextStyle {
+        return Typography(
+            h1 = TextStyle(
+                fontFamily = PoppinsSemiBold,
+                fontSize = textUnit,
+                textAlign = align
+            )
+        ).h1
+    }
+
+    fun poppinsRegularStyle(textUnit: TextUnit, align: TextAlign? = TextAlign.Start): TextStyle {
+        return Typography(
+            h1 = TextStyle(
+                fontFamily = PoppinsRegular,
+                fontSize = textUnit,
+                textAlign = align
+            )
+        ).h1
+    }
+}
+
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+)
+
+val PoppinsMedium = FontFamily(
+    Font(R.font.poppins_medium)
+)
+
+val PoppinsSemiBold = FontFamily(
+    Font(R.font.poppins_semi_bold)
+)
+
+val PoppinsRegular = FontFamily(
+    Font(R.font.poppins_regular)
 )
